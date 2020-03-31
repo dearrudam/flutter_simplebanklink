0aea330d6f48600b7261f27327da63f88d85e0d0
Maximillian Arruda <marruda@opentext.com>
2020-03-31 02:14:17 -0300

    Primeiro projeto Flutter concluído

---
+++ lib/screens/transferencias/formulario.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simplebanklink/components/editor.dart';
+import 'package:flutter_simplebanklink/models/transferencia.dart';
+
+const _titleAppBar = 'Criando Transferência';
+const _rotuloCampoNumeroConta = 'Número da Conta';
+const _dicaCampoNumeroConta = '0000';
+const _rotuloCampoValor = 'Valor';
+const _dicaCampoValor = '0.00';
+const _textoBotaoConfirmar = 'Confirmar';
+
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return FormularioTransferenciaState();
+  }
+}
+
+class FormularioTransferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titleAppBar),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Editor(
+              controlador: _controladorCampoNumeroConta,
+              rotulo: _rotuloCampoNumeroConta,
+              dica: _dicaCampoNumeroConta,
+            ),
+            Editor(
+              controlador: _controladorCampoValor,
+              rotulo: _rotuloCampoValor,
+              dica: _dicaCampoValor,
+              icone: Icons.monetization_on,
+            ),
+            RaisedButton(
+              child: Text(_textoBotaoConfirmar),
+              onPressed: () => _criarTransferencia(context),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _criarTransferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+    if (numeroConta != null && valor != null) {
+      final Transferencia transferenciaCriada =
+          Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+}
---
+++ lib/screens/transferencias/lista.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simplebanklink/models/transferencia.dart';
+import 'package:flutter_simplebanklink/screens/transferencias/formulario.dart';
+
+const _titleAppBar = 'Transferências';
+
+class ListaTransferencias extends StatefulWidget {
+  final List<Transferencia> _transferencias = List();
+
+  @override
+  State<StatefulWidget> createState() {
+    return ListaTranferenciasState();
+  }
+}
+
+class ListaTranferenciasState extends State<ListaTransferencias> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titleAppBar),
+      ),
+      body: ListView.builder(
+        itemCount: widget._transferencias.length,
+        itemBuilder: (context, indice) {
+          final Transferencia transferencia = widget._transferencias[indice];
+          return ItemTransferencia(transferencia);
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(
+            builder: (context) {
+              return FormularioTransferencia();
+            },
+          )).then(_adicionar);
+        },
+      ),
+    );
+  }
+
+  void _adicionar(transferenciaRecebida) {
+    if (transferenciaRecebida != null) {
+      setState(() {
+        widget._transferencias.add(transferenciaRecebida);
+      });
+    }
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
